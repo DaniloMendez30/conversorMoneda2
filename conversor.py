649e4b8257fd6365429ee5f23175e038067c378f
DaniloMendez30 <danilo.mendez@frubana.com>
2022-07-29 20:08:43 -0500

    cambios en codigo conversor.py

--- conversor.py
+++ conversor.py
@@ -1,21 +1,40 @@
 
-moneda = input("Seleciona la moneda de tus pais 1 = Colombia ; 2 = México : ")
-moneda = int(moneda)
 
-if moneda == 1 :
-    pesos = input("Cuantos pesos colombianos tienes?: ")
-    pesos = float(pesos)
-    valor_dolar = 4402
-    dolares = round(pesos / valor_dolar, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dolares" )
+menu = """
+Bienvenido al conversor de monedas multipais
 
-elif moneda == 2 :
+1-Pesos Mexicanos
+2-Pesos Colombianos
+3-Pesos Argentinos
 
-    pesos_mexicanos = input("Cuantos pesos Mexicanos tienes?: ")
-    pesos_mexicanos = float(pesos_mexicanos)
-    valor_dolar = 0.049
-    dolares = round(pesos_mexicanos / valor_dolar, 2)
-    dolares = str(dolares)
-    print("Tienes $" + dolares + " dolares" )
+Elige una opción: 
 
+"""
+
+opcion = int(input(menu))
+
+if opcion == 1: 
+	pesos = input('¿Cuántos pesos mexicanos tienes?: ')
+	pesos = float(pesos)
+	tipo_de_cambio = 21.5
+elif opcion == 2: 
+	pesos = input('¿Cuántos pesos colombianos tienes?: ')
+	pesos = float(pesos)
+	tipo_de_cambio = 3715.01
+elif opcion == 3: 
+	
+	pesos = input('¿Cuántos pesos argentinos tienes?: ')
+	pesos = float(pesos)
+	tipo_de_cambio = 74.44
+	print('Escribe una opción correcta: ')
+
+
+
+dolares = pesos / tipo_de_cambio
+
+dolares = round(dolares, 2)
+
+dolares = str(dolares)
+
+
+print('Tienes $' + dolares +' dólares')
