0f7e8b845929094341762c15c7704d0e3be129a4
DaniloMendez30 <danilo.mendez@frubana.com>
2022-08-01 18:50:28 -0500

    se agrega archivo polindromo

---
+++ palindromo.py
@@ -0,0 +1,19 @@
+def palindromo(palabra):
+    palabra = palabra.replace(' ', '')
+    palabra = palabra.lower()
+    palabra_invertida = palabra[::-1]
+    if palabra == palabra_invertida:
+        return True
+    else: 
+        return False
+
+def run(): #Es buan practica iniciar el codigo con un funcion y asi no va leer linia po linia
+    palabra = input("Escribe una palabla: ")
+    es_palindromo = palindromo(palabra)
+    if es_palindromo == True:
+        print("Es palindromo")
+    else: 
+        print("No es palind") 
+    
+if __name__ == "__main__":
+    run()
