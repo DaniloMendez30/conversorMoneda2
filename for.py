903386e93d02c0347a77e5e355720e6327ac25b4
DaniloMendez30 <danilo.mendez@frubana.com>
2022-08-02 10:01:00 -0500

    entendiendo el ciclo for

--- for.py
+++ for.py
@@ -1,9 +1,8 @@
 def run():
-    numero = 0
-    LIMITE = 10
-    while numero < LIMITE:
-        print(numero)
-        numero += 1
+    for numero in range(10):
+        print(11 * numero)
 
 if __name__ == '__main__':
     run()
+
+
