70d3b27022c81a29ec7f807c1ef595c9303a5d44
DaniloMendez30 <danilo.mendez@frubana.com>
2022-08-02 09:27:02 -0500

    ya terminado el codigo de bucle while

--- bloques.py
+++ bloques.py
@@ -1,5 +1,12 @@
-contador = 0
+def run():
+    contador = 0
+    LIMITE = 1000 # cuando tu colocar en mayusculas una variable es se convierte de tipoo constante es decir que no va cambiar
+    potencia = 2**contador
 
-while contador < 10:
-    print("2 elevado a " + str(contador) + " es igual a " + str(contador**2) )
-    contador = contador + 1
+    while potencia < LIMITE :
+        print("2 elevado a " + str(contador) + " es igual a " + str(potencia) )
+        contador = contador + 1
+        potencia = 2**contador
+
+if __name__ == '__main__':
+    run()
