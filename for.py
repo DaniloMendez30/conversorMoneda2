c7536545f19938a8ce8d65d025ba7cfe71d2acca
DaniloMendez30 <danilo.mendez@frubana.com>
2022-08-03 09:03:21 -0500

    recorrer cadenas de caractereres

--- for.py
+++ for.py
@@ -1,8 +1,19 @@
+#def run():
+#    for numero in range(10):
+#        print(12 * numero)
+
+#if __name__ == '__main__':
+#    run()
+
+
+#Recorer una cadena de caractereres
+
 def run():
-    for numero in range(10):
-        print(11 * numero)
+     nombre = input("Escribe tu noombre: ")
+
+     for letra in nombre:
+         print(letra.upper())
 
 if __name__ == '__main__':
     run()
-
 
