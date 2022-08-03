d488683aeb8b60e741c7844cab01ca723a5fa032
DaniloMendez30 <danilo.mendez@frubana.com>
2022-08-03 09:16:58 -0500

    entendiendo el break y continue

--- for.py
+++ for.py
@@ -9,10 +9,21 @@
 #Recorer una cadena de caractereres
 
 def run():
-     nombre = input("Escribe tu noombre: ")
+    # for i in range(10000):
+    #    print(i)
+    #    if i == 5678:
+    #        break
 
-     for letra in nombre:
-         print(letra.upper())
+    #for contador in range(1000):
+    #    if contador % 2 != 0:
+    #        continue
+    #    print(contador)
+
+    texto= input('Escribe un texto: ')
+    for letra in texto:
+        if letra == 'o':
+            break
+        print(letra)
 
 if __name__ == '__main__':
     run()
