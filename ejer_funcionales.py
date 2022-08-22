238da0c6397c8a533fd453c987d919e64fd5af4a
DaniloMendez30 <danilo.mendez@frubana.com>
2022-08-22 10:49:20 -0500

    nuevos ejercicios de python1

--- ejer_funcionales.py
+++ ejer_funcionales.py
@@ -2,31 +2,59 @@
 
 
 def run():
-   
-   initial = 0
+    lista1 = [23, 45, 67, 20]
+    lista2 = [47, 67, 87, 20]
+    iguales = []
 
-   number1 = int(input("digita el primer numero: "))
-   initial = number1
+    for i in lista1:
+        for j in lista2:
+            if i == j:
+                iguales.append(i)
+            else: continue
 
-   number2 = int(input("digita el digita el segundo numero: "))
-   
-
-   if initial > number2:
-       pass
-   else: 
-       initial = number2
-
-   number3 = int(input("digita el digita el tercer numero: "))
+    print("las listas coinciden en: " + str(iguales))
+            
    
 
-   if initial > number3:
-        print("el numero mayor de los tres es " + str(initial))
-   else: 
-       initial = number3
-       print("el numero mayor de los tres es " + str(initial))
 
+
+
+    # lista = [5,10,20,30]
+    # cont = 0
+    # multi = 1
+    
+
+    # for i in lista:
+    #     cont = cont + i 
+    #     multi = multi * i
+    
+    # print("la suma de la lista es " + str(cont))
+    # print("la multipicacion de la lista es " + str(multi))
 
     
+   
+#    initial = 0
+
+#    number1 = int(input("digita el primer numero: "))
+#    initial = number1
+
+#    number2 = int(input("digita el digita el segundo numero: "))
+   
+
+#    if initial > number2:
+#        pass
+#    else: 
+#        initial = number2
+
+#    number3 = int(input("digita el digita el tercer numero: "))
+   
+
+#    if initial > number3:
+#         print("el numero mayor de los tres es " + str(initial))
+#    else: 
+#        initial = number3
+#        print("el numero mayor de los tres es " + str(initial))
+
 
 if __name__ == "__main__":
     run()
